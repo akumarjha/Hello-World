3ba159e069282b31087c21f4e242e7385be29f0d
VI\inajha <jhaashu55@gmail.com>
2019-01-22 11:56:13 +0530

    bbb

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i =0; i<=10;i++){
+			for(int j=0; j<=i;j++)
+			{
+				System.out.println(j);
+			}
 			System.out.println("Hello World...."+i);
 		}
 	}
